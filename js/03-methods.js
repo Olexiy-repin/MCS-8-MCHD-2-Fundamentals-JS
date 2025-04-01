d72f8e786a331bbb5353a77ac3954d10ce87023c
Olexiy-repin <repinlyoha@gmail.com>
2025-04-01 13:42:59 +0300

    preparation

---
+++ js/03-methods.js
@@ -0,0 +1,56 @@
+/*
+ * Методи масиву
+ *
+ * - join
+ * - split (Метод рядка)
+ * - slice
+ * - concat
+ * - indexOf
+ * - push/pop
+ */
+
+//~ Використання метода join()
+
+// const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS'];
+// const result = null;
+
+// console.log('courses:', courses);
+// console.log('result:', result);
+
+//~ Використання метода split()
+
+// const phoneNumbers = '+380961111111,+380961111111,+380961111111,+380961111111';
+// const phoneNumbersArr = null;
+
+// console.log('phoneNumbers:', phoneNumbers);
+// console.log('phoneNumbersArr:', phoneNumbersArr);
+
+//~ Використання метода slice()
+
+// const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS'];
+// const coursesCopy = null;
+
+// console.log('courses:', courses);
+// console.log('coursesCopy:', coursesCopy);
+
+//~ Використання метода concat()
+
+// const oldCourses = ['HTML', 'CSS'];
+// const newCourses = ['JavaScript', 'React', 'NodeJS'];
+// const allCourses = null;
+
+// console.log('oldCourses:', oldCourses);
+// console.log('newCourses:', newCourses);
+// console.log('allCourses:', allCourses);
+
+//~ Використання метода indexOf()
+
+// const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS'];
+
+// console.log('courses:', courses);
+
+//~ Використання методів push()/pop() та unshift()/shift()
+
+// const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS'];
+
+// console.log('courses:', courses);
